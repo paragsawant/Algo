45818d4fdefc3f42d50c35ca0c642ab1d271d76c
Parag Sawant <paragpps@gmail.com>
2017-06-19 23:03:59 -0700

    update

---
+++ TestConsoleApplication/Array_LeftRotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class Array_LeftRotation
+    {
+        static void Start(String[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = k; i < n; i++)
+            {
+                sb.Append(a[i].ToString());
+                sb.Append(" ");
+            }
+            for (int j = 0; j < k; j++)
+            {
+                sb.Append(a[j].ToString());
+                sb.Append(" ");
+            }
+            Console.Write(sb.ToString().Trim());
+        }
+    }
+}
---
+++ TestConsoleApplication/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    public static class StringExtension
+    {
+        public static int WordCount(this string input)
+        {
+            return input.Split(new char[] { ' ', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string LogError(this string input)
+        {
+            return "Exception : "+input;
+        }
+    }
+}
