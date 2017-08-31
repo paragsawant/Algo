c8c23c8804c8fa7e770bb9305a52e3d07d72903f
snehaps0213 <snehaps0213@gmail.com>
2017-08-30 23:28:10 -0700

    pushing test class

---
+++ InterviewPractice/InterviewTopics/InterviewTopics/StacksProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewTopics
+{
+    public class StacksProblem
+    {
+
+        public int GetEvaluatedValue(string expression)
+        {
+            foreach (var str in expression)
+            {
+                if ((int)str > 47 && (int)str < 58)
+                {
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ InterviewPractice/InterviewTopics/UnitTestProject/StackTest.cs
@@ -0,0 +1,22 @@
+﻿using InterviewTopics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void TestSearchElement()
+        {
+            StacksProblem sp = new StacksProblem();
+            sp.GetEvaluatedValue("5*5+2+3*5");
+        }
+
+    }
+}
