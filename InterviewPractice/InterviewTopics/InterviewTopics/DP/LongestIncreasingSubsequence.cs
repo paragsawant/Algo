bef8f9285409f492e363901b3e66c8ea5ead7d55
snehaps0213 <snehaps0213@gmail.com>
2017-10-04 16:38:51 -0700

    LIS

---
+++ InterviewPractice/InterviewTopics/InterviewTopics/DP/LongestIncreasingSubsequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewTopics.DP
+{
+    public class LongestIncreasingSubsequence
+    {
+        public int LIS(int[] inputs)
+        {
+            int inputLength = inputs.Length;
+            int returnValue = 0;
+            if (inputLength == 1)
+            {
+                return 1;
+            }
+
+            int[] lis = new int[inputLength];
+            for (int x = 0; x < inputLength; x++)
+            {
+                lis[x] = 1;
+            }
+            for (int i = 1; i < inputLength; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (inputs[i] > inputs[j] && lis[j] + 1 > lis[i])
+                    {
+                        lis[i] = lis[j] + 1;
+                        returnValue = returnValue > lis[i] ? returnValue : lis[i];
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ InterviewPractice/InterviewTopics/UnitTestProject/LongestIncreasingSubsequenceTest.cs
@@ -0,0 +1,23 @@
+﻿using InterviewTopics.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class LongestIncreasingSubsequenceTest
+    {
+        [TestMethod]
+        public void LISTest()
+        {
+            LongestIncreasingSubsequence _lis = new LongestIncreasingSubsequence();
+            int[] arr = new int[] { 10, 22, 9, 33, 21, 50, 41, 60 };
+            int value = _lis.LIS(arr);
+            Assert.AreEqual(5, value);
+        }
+    }
+}
