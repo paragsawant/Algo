336c6bf2e44b207aed3e8c0dfda7c8bffd318ca0
snehaps0213 <snehaps0213@gmail.com>
2017-10-10 21:21:17 -0700

    Some practice

---
+++ ABGlobal/ABGlobal/ABGlobal/Model/UserOrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABGlobal.Model
+{
+    public class UserOrderDetails
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+        public UserOrderDetails()
+        { }
+    }
+}
---
+++ ABGlobal/ABGlobal/ABGlobal/Program.cs
@@ -0,0 +1,66 @@
+﻿using ABGlobal.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABGlobal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, UserOrderDetails> _userDetails = new Dictionary<int, UserOrderDetails>();
+            //reading user details csv and adding that to dictonary
+            int userCount = 0;
+            string userDetailsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"File\UserDetails.csv");
+            string orderDetailsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"File\OrderDetails.csv");
+            bool isUserDetailsHeader = true;
+            foreach (var line in File.ReadLines(userDetailsPath))
+            {
+                if (isUserDetailsHeader)
+                {
+                    isUserDetailsHeader = false;
+                    continue;
+                }
+
+                string[] lineDetails = line.Split(',');
+                ++userCount;
+                var theLine = new UserOrderDetails()
+                {
+                    UserId = Convert.ToInt32(lineDetails[0]),
+                    UserName = lineDetails[1],
+                    RegistrationDate = Convert.ToDateTime(lineDetails[2]),
+                    OrderDetails = new List<OrderDetails>()
+                };
+                _userDetails.Add(theLine.UserId, theLine);
+            }
+            bool isOrderDetailsHeader = true;
+            int orderCount = 0;
+            foreach (var line in File.ReadLines(orderDetailsPath))
+            {
+                if (isOrderDetailsHeader)
+                {
+                    isOrderDetailsHeader = false;
+                    continue;
+                }
+
+                string[] lineDetails = line.Split(',');
+                ++orderCount;
+                UserOrderDetails userDetails = new UserOrderDetails();
+                if (_userDetails.ContainsKey(Convert.ToInt32(lineDetails[0])))
+                {
+                    OrderDetails orderDetails = new OrderDetails();
+                    orderDetails.OrderId = Convert.ToInt32(lineDetails[1]);
+                    orderDetails.OrderDetail = lineDetails[3];
+                    orderDetails.OrderDate = Convert.ToDateTime(lineDetails[2]);
+                    orderDetails.OrderQuantity = Convert.ToInt32(lineDetails[4]);
+                    _userDetails[Convert.ToInt32(lineDetails[0])].OrderDetails.Add(orderDetails);
+                }
+            }
+        }
+    }
+}
